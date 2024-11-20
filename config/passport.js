b03faad1445bb4821b8a94c0c0d18a48d0b88704
JmmonJeremy <suc19002@byui.edu>
2024-11-20 14:58:12 -0800

    Revert "Added a log in passport."
    This reverts commit 0c886c39d6d8479e48e82eb767a1ff708807b94e.

--- config/passport.js
+++ config/passport.js
@@ -131,7 +131,6 @@ module.exports = function (passport) {
   // from https://www.passportjs.org/tutorials/google/session/ 
   // done was used to replace cb (short fro callback) in the code
  passport.serializeUser(async (wrappedUser, done) => {
-    console.log("Deserializing user ID:", id);
     // Save only the user ID and accessToken   
     done(null, { id: wrappedUser.user._id, accessToken: wrappedUser.accessToken });   
   });
